1b253434b0c575b8a5c5b0dfd32d7bbf4714611c
Proyetei <proyeteireavue@gmail.com>
2025-04-04 06:02:04 -0400

    add new splash screen

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -1,11 +1,13 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import anime from 'animejs';
-import { TbHexagonLetterP } from "react-icons/tb";
-interface SplashScreenProps{
-  finishloading: () => void
+import Image from 'next/image';
+
+interface SplashScreenProps {
+  finishloading: () => void;
 }
-const SplashScreen: React.FC<SplashScreenProps> = ({finishloading}) => {
+
+const SplashScreen: React.FC<SplashScreenProps> = ({ finishloading }) => {
   const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
@@ -16,7 +18,7 @@ const SplashScreen: React.FC<SplashScreenProps> = ({finishloading}) => {
       easing: 'easeInOutQuad',
       complete: () => finishloading(),
     });
-  }, []);
+  }, [finishloading]);
 
   if (!isVisible) return null;
 
@@ -25,7 +27,13 @@ const SplashScreen: React.FC<SplashScreenProps> = ({finishloading}) => {
       id="splash-screen"
       className="min-h-screen flex items-center justify-center bg-slate-900 z-50"
     >
-      <h1 className="text-7xl text-indigo-100 animate-bounce"> <TbHexagonLetterP /> </h1>
+      <Image
+        src="/dp.png"
+        alt="dp"
+        width={50}
+        height={50}
+        className="animate-bounce rounded-full"
+      />
     </div>
   );
 };
